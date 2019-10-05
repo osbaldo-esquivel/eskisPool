4786ededbcf102230c99ac96a75b46dbe9054493
Osbaldo Esquivel <oesquivel@noip.com>
2019-10-05 14:28:10 -0700

    Add logo, fix navigation, and fix welcome page

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,45 +1,36 @@
 <nav class="navbar navbar-expand-md topnav shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/dashboard') }}">
-            {{ config('app.name', 'Eskis Pool') }}
+            <img src="/images/logo_200x200.png" height="60" width="60">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                @guest
+        <ul class="navbar-nav ml-auto">
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <div class="ml-auto">
-                        <a href="{{ route('pool') }}">Pool</a>
-                        <a href="{{ route('dashboard') }}">My Picks</a>
-                    </div>
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                @endguest
-            </ul>
-        </div>
+                @endif
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('pool') }}">Pool</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('dashboard') }}">My Picks</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            @endguest
+        </ul>
     </div>
 </nav>
