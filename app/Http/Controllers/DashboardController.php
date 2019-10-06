0ff957b1a8779bf2bcb99b3260fa5b0815ef01fe
Osbaldo Esquivel <oesquivel@noip.com>
2019-10-05 21:20:33 -0700

    Add locked logic back

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -25,6 +25,9 @@ class DashboardController extends Controller
     {
         return view('dashboard', [
             "games" => new Game(config('pool.week')),
+            "locked" => [
+                "locked" => Carbon::parse('2019-10-03T17:00:00+00:00', 'America/Los_Angeles')->isPast()
+            ],
         ]);
     }
 }
