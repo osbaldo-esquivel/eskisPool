baaa679a4e245519fc68781bc0a73a935d449ad5
Osbaldo Esquivel <oesquivel@noip.com>
2019-10-09 20:27:17 -0700

    Update landing page and add Procfile

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,11 +6,11 @@
         <ul class="navbar-nav ml-auto">
             @guest
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <a class="nav-link" href="{{ route('login') }}">Sign in</a>
                 </li>
                 @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        <a class="nav-link" href="{{ route('register') }}">Sign up</a>
                     </li>
                 @endif
             @else
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,23 @@
 
 @section('content')
 <div class="content">
-    <div class="title m-b-md">
-        Click login to view pool.
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Rules</h4>
+                </div>
+                <div class="card-body text-center">
+                    <p class="card-text">Winner takes the pool <img src="/images/money1.png" width="40" height="40"></p>
+                    <p class="card-text">Picks can be submitted at any time up until 5 PM PST on Thursdays when there is TNF, otherwise, they are due Sunday by 6:00 AM PST.</p>
+                    <p class="card-text">In case there is a tie in picks, the tiebreaker will be whoever is closest to the total score of the MNF game. If there is a tie with scores, the pool will be split between the winners.</p>
+                    <p class="card-text">There is no fee, but if you want to buy David a beer, that's fine.</p>
+                    <button class="btn btn-secondary">
+                        <a href={{ url('/dashboard') }}>Let's go</a>
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
