cc9bc3bb25a5b6a448662ebabca49e3068795fe6
Osbaldo Esquivel <oesquivel@noip.com>
2021-04-05 21:14:14 -0700

    Get timer working and start working on sound

--- resources/views/games/power-hour.blade.php
+++ resources/views/games/power-hour.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="card">
             <div class="eski-timer">
-                <p id="timer"></p>
+                <p id="timer">0h 0m 0s</p>
             </div>
             <div class="btn-group">
                 <button class="btn btn-success" onClick="setTimer(60)">
@@ -19,12 +19,13 @@
                 </button>
             </div>
             <div class="btn-group">
-                <button class="btn btn-primary timerstart" id="start" onClick="startTimer()">
+                <button class="btn btn-primary" id="start" disabled>
                     Start
                 </button>
-                <button class="btn btn-primary timereset" id="reset" onClick="resetTimer()">
+                <button class="btn btn-primary" id="reset">
                     Reset
                 </button>
+                <audio id="audio" src="/sounds/bell.wav"></audio>
             </div>
         </div>
     </div>
@@ -33,45 +34,72 @@
 
 @section('scripts')
 <script>
-    time = 0;
+var time = 0;
+var start_button = document.getElementById('start');
+var reset_button = document.getElementById('reset');
+var timer = document.getElementById('timer');
+var clicked = false;
 
-    function setTimer(minutes) {
-        time = minutes;
-    }
+function setTimer(minutes) {
+    time = minutes;
 
-    function startTimer() {
-        var start_button = document.getElementById('start');
-        var reset_button = document.getElementById('reset');
-        var timer = document.getElementById('timer');
+    time == 60
+        ? timer.innerText = '0h 60m 0s'
+        : timer.innerText = '1h 40m 0s';
+
+    start_button.disabled = false;
+}
+
+start_button.addEventListener('click', function() {
+    start_button.disabled = true;
+    
+    var nowDate = new Date();
+
+    var countDownDate = new Date(nowDate.getTime() + (time * 60000)).getTime();
+    
+    var x = setInterval(function() {    
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         
-        start_button.innerText = 'Pause';
-
-        start_button.addEventListener('click', function() {
-            // TODO
-        });
-
-        reset_button.addEventListener('click', function() {
+        timer.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+        
+        if (distance < 0) {
             clearInterval(x);
-        });
 
-        var nowDate = new Date();
+            timer.innerHTML = "Nice.";
+        }
 
-        var countDownDate = new Date(nowDate.getTime() + (time * 60000)).getTime();
+        var y = setInterval(function() {
+            var nowDate1 = new Date();
+
+            var countDownDate1 = new Date(nowDate1.getTime() + 60000).getTime();
+
+            var distance1 = countDownDate1 - nowDate1;
         
-        var x = setInterval(function() {    
-            var now = new Date().getTime();
-            var distance = countDownDate - now;
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            
-            timer.innerHTML = minutes + "m " + seconds + "s ";
-            
-            if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("timer").innerHTML = "YOU MADE IT";
+            if (distance1 < 0) {
+                clearInterval(y);
+
+                document.getElementById('audio').play();
             }
         }, 1000);
-    }
+    }, 1000);
+
+    reset_button.addEventListener('click', function() {
+        clearInterval(x);
+
+        timer.innerText = '0h 0m 0s';
+
+        start_button.disabled = true;
+    });
+});
+
+function play() {
+    var audio = document.getElementById('audio');
+
+    audio.play();
+}
 </script>
 @endsection
